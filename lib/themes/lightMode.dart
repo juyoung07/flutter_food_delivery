e87acc22ce515336b3fca43f81b33d779bf4ef73
Yun juyoung <yunju5090@gmail.com>
2024-09-26 22:28:45 +0900

    feat: dark mode & light mode

--- lib/themes/lightMode.dart
+++ lib/themes/lightMode.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
 
+ThemeData lightMode = ThemeData(
+    colorScheme: ColorScheme.light(
+  surface: Colors.grey.shade300, // background
+  primary: Colors.grey.shade500,
+  secondary: Colors.grey.shade100,
+  tertiary: Colors.white,
+  inversePrimary: Colors.grey.shade700,
+));
---
+++ lib/themes/themeProvider.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_food_delivery/themes/darkMode.dart';
+import 'package:flutter_food_delivery/themes/lightMode.dart';
+
+class ThemeProvider with ChangeNotifier {
+  ThemeData _themeData = lightMode;
+
+  ThemeData get themeData => _themeData;
+
+  bool get isDarkMode => _themeData == darkMode;
+
+  set themeData(ThemeData themeData) {
+    _themeData = themeData;
+    notifyListeners();
+  }
+
+  void toggleTheme() {
+    if (_themeData == lightMode) {
+      themeData = darkMode;
+    } else {
+      themeData = lightMode;
+    }
+  }
+}
